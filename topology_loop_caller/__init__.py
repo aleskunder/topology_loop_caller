035744a97008d87f24acf8d9029da4d1ca602832
aleskunder <alesk@DESKTOP-GTSGR83.localdomain>
2023-02-22 21:03:37 +0400

    Organized data by code/input_data/output_data/example_notebooks; created setup.py, __init__.py

---
+++ topology_loop_caller/__init__.py
@@ -0,0 +1,27 @@
+__title__ = "topology_loop_caller"
+__version__ = "1.0.0"
+__author__ = "Alexander Kuznetsov"
+__email__ = "alexander.kuznetsov.bioinf@gmail.com"
+__description__ = (
+    "Persistent homology - based pipeline of feature generation and loop calling"
+)
+
+__url__ = "https://github.com/aleskunder/topology_loop_caller"
+
+__keywords__ = [
+    "topology",
+    "chromatin",
+    "Hi-C",
+    "bioinformatics",
+    "ML",
+]
+
+__classifiers__ = [
+    "Intended Audience :: Science/Research",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Julia",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Scientific/Engineering",
+]

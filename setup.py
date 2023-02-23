d60e66ddc90256dda444c7adeaa02888ac174d5d
aleskunder <alesk@DESKTOP-GTSGR83.localdomain>
2023-02-23 12:27:21 +0400

    setup.py edits

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from setuptools import setup, find_packages
 with open("requirements.txt", "r") as f:
     install_requires = f.read().split("\n")
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 # Read __version__ variable
 __version__ = "0.0.0"
 exec(open("topology_loop_caller/__init__.py").read())
@@ -13,12 +16,20 @@ setup(
     name="topology_loop_caller",
     version=__version__,
     packages=find_packages(),
-    url="",
-    license="",
-    author="Alexander_Kuznetsov",
+    url="https://github.com/aleskunder/topology_loop_caller",
+    license="",  # Add after clarif
+    author="Khrameeva Lab",  # Clarif
     author_email="alexander.kuznetsov.bioinf@gmail.com",
     description="Persistent homology - based pipeline of feature generation and loop calling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=install_requires,
     package_dir={"topology_loop_caller": "topology_loop_caller"},
     include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
